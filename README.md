2ddad816e8f98deb45db676d5aad71c78f48a060
yoyo <maonome@gmail.com>
2024-12-31 11:45:36 +0800

    2024.12.31 post

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# ThisDay Blog
+# Blog
--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
 	site_name: 'Just Thora',
-	title: 'ThisDay',
+	title: 'Just Thora',
 	description: `I've stayed in my comfort zone; now it's time to step out.`,
 	author: 'Thora',
 	locale_region: 'zh-tw',
